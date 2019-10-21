d1e5f276a69f84600b3e87ae83f42bf6223fd2de
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-10-21 21:39:54 +0900

    Create routing ( GET/POST request)

---
+++ server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"net/http"
+)
+
+func main()  {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1234"))
+
+	e.GET("/users/:id", getUser)
+	e.GET("/show", show)
+	//e.PUT("/users/:id", updateUser)
+	//e.DELETE("/users/:id", deleteUser)
+
+	e.POST("/save", save)
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+func show(c echo.Context) error {
+	// Get team and member from the query string
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "tea,:" + team + ", member:" + member)
+}
+
+func save(c echo.Context) error {
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name:" + name + ", email:" + email)
+}
