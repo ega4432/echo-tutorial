f00e7d61c0a74d3c48e404abb6f4b4a9bee3b869
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-10-22 23:39:05 +0900

    POST method request

--- server.go
+++ server.go
@@ -2,25 +2,37 @@ package main
 
 import (
 	"github.com/labstack/echo"
+	"io"
 	"net/http"
+	"os"
 )
 
-func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":1234"))
+const helloText string = `Hello, World from echo!
+echo is high performance, extensible, minimalist Go web framework`
 
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, helloText)
+}
+
+func main() {
+	// Echo Instance
+	e := echo.New()
+
+	// Route
+	e.GET("/", hello)
 	e.GET("/users/:id", getUser)
 	e.GET("/show", show)
+	e.POST("/save", save)
 	//e.PUT("/users/:id", updateUser)
 	//e.DELETE("/users/:id", deleteUser)
 
-	e.POST("/save", save)
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func getUser(c echo.Context) error {
+	// User ID from path `users/:id`
 	id := c.Param("id")
 	return c.String(http.StatusOK, id)
 }
@@ -33,7 +45,32 @@ func show(c echo.Context) error {
 }
 
 func save(c echo.Context) error {
+	// get name
 	name := c.FormValue("name")
-	email := c.FormValue("email")
-	return c.String(http.StatusOK, "name:"+name+", email:"+email)
+	// get avatar
+	avatar, err := c.FormFile("avatar")
+	if err != nil {
+		return err
+	}
+
+	// Source
+	src, err := avatar.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	// Destination
+	dst, err := os.Create(avatar.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return c.HTML(http.StatusOK, "<b>Thank you! "+name+"</b>")
 }
